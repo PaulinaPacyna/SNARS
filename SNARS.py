dec9cbe09a1f930e3b1c8ef402e2f902fe792118
PaulinaPacyna <pacyna1998@gmail.com>
2021-11-08 09:19:49 +0100

    .

---
+++ SNARS.py
@@ -0,0 +1,39 @@
+import networkx as nx
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+
+
+class Snars:
+    def __init__(self, Graph: nx.Graph):
+        self.G = Graph
+        self.degrees = dict(self.G.degree)
+        self.degree_list = list(self.degrees.values())
+
+    def plot_log_log_hist(self):
+        loglog_hist = plt.hist(self.degree_list, bins=np.exp(np.linspace(np.log(min(self.degree_list)),
+                                                                         np.log(max(self.degree_list)), 20)))
+        plt.yscale('log')
+        plt.xscale('log')
+        return loglog_hist
+
+    def survival(self, x: float) -> float:
+        cdf = len(list(filter(lambda v: v < x, self.degree_list))) / len(self.degree_list)
+        # nr of observations less than x/nr of observations
+        return 1 - cdf
+
+    def plot_survival(self):
+        x_to_plot = np.unique(self.degree_list)
+        y_to_plot = [self.survival(x) for x in x_to_plot]
+        plt.plot(x_to_plot, y_to_plot)
+
+    def est_alpha_lin_regression(self):
+        bins = self.plot_log_log_hist() # the function returns a list of bins and counts
+        log_y = bins[0].reshape(-1, 1)
+        log_x = np.array([(a + b) / 2 for a, b in zip(bins[1][:-1], bins[1][1:], )]).reshape(-1,1)  # middles of the intervals
+        model = LinearRegression(fit_intercept=True).fit(log_x, log_y)
+        return model.coef_, model.intercept_
+
+    def mle(self):
+        x_min = min(self.degree_list)
+        return 1 + len(self.degree_list) / (np.log(x_min) + sum(np.log(obs)))
